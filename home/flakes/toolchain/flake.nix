c68dd5bbb9045c29df6069cb97917d7baf727b1e
Thomas Sauvajon <thomas.sauvajon@proton.me>
2026-04-30 15:39:32 +0200

    Migrate from nixos to nixpkgs to get latest version of tools

--- home/flakes/toolchain/flake.nix
+++ home/flakes/toolchain/flake.nix
@@ -2,7 +2,7 @@
   description = "Personal dev toolchain for OpenCode + worktrees";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
