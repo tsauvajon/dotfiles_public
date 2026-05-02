d12e1d7e216a1411aca8227d55a77d6a1f889e3b
Thomas Sauvajon <thomas.sauvajon@proton.me>
2026-05-02 15:22:24 +0200

    refactor: source theme files directly from submodules
    - Mako: symlink catppuccin-mocha-mauve from submodule to config.d/, remove manual color definitions from config
    - Rofi: replace catppuccin.rasi with symlink to submodule's mocha theme file
    - FZF: source fish snippet from catppuccin/fzf submodule instead of manually setting FZF_DEFAULT_OPTS

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -4,7 +4,8 @@ set -gx VISUAL hx
 set -x MANROFFOPT -c
 set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"
 set -x BAT_THEME "Catppuccin Mocha"
-set -x FZF_DEFAULT_OPTS "--color=bg+:#313244,bg:#1E1E2E,spinner:#F5E0DC,hl:#F38BA8 --color=fg:#CDD6F4,header:#F38BA8,info:#CBA6F7,pointer:#F5E0DC --color=marker:#B4BEFE,fg+:#CDD6F4,prompt:#CBA6F7,hl+:#F38BA8 --color=selected-bg:#45475A --color=border:#6C7086,label:#CDD6F4"
+# Source fzf catppuccin mocha theme from submodule
+test -f ~/.config/fzf/catppuccin/themes/catppuccin-fzf-mocha.fish && source ~/.config/fzf/catppuccin/themes/catppuccin-fzf-mocha.fish
 
 # Enable Wayland support for different applications
 if [ "$XDG_SESSION_TYPE" = wayland ]
