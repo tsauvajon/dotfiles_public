9efad1b54910efb635ee0e06a0bc8f7ba5c1e2f8
Thomas Sauvajon <thomas.sauvajon@proton.me>
2026-05-26 12:02:58 +0200

    Fix kache installation

--- pkgs/kache/default.nix
+++ pkgs/kache/default.nix
@@ -1,34 +1,61 @@
 {
   lib,
-  rustPlatform,
-  apple-sdk_15,
   stdenv,
-  src,
+  fetchurl,
 }:
 
-rustPlatform.buildRustPackage {
+let
+  sources = {
+    aarch64-darwin = {
+      target = "aarch64-apple-darwin";
+      hash = "sha256-g3UFzJ6FEwFbp58YmxVL3D5DRgA06hrgFyJ5CHF3rNc=";
+    };
+    x86_64-darwin = {
+      target = "x86_64-apple-darwin";
+      hash = "sha256-GPcxZYlXEeF8NSNPOPasLk15K9aUPD+fJrSGm7NqfVE=";
+    };
+    aarch64-linux = {
+      target = "aarch64-unknown-linux-musl";
+      hash = "sha256-l6+LbxzaXqmBBYkmKB2belmy9kqs2GcX9GGD3HzLimA=";
+    };
+    x86_64-linux = {
+      target = "x86_64-unknown-linux-musl";
+      hash = "sha256-PKOctuBD0afkq1T3V1YDTAyUq3A5Qpm3Xx6Emlq3wYQ=";
+    };
+  };
+
+  source =
+    sources.${stdenv.hostPlatform.system}
+      or (throw "kache is not packaged for ${stdenv.hostPlatform.system}");
+in
+stdenv.mkDerivation rec {
   pname = "kache";
   version = "0.3.1";
 
-  inherit src;
+  src = fetchurl {
+    url = "https://github.com/kunobi-ninja/kache/releases/download/v${version}/kache-${source.target}.tar.gz";
+    inherit (source) hash;
+  };
 
-  cargoHash = "sha256-s61zrPFP+BdtHj0vGic8LDGBuR6vydPWksKvRXtd1cI=";
+  sourceRoot = ".";
 
-  buildInputs = lib.optionals stdenv.hostPlatform.isDarwin [
-    apple-sdk_15
-  ];
+  dontConfigure = true;
+  dontBuild = true;
 
-  checkFlags = lib.optionals stdenv.hostPlatform.isDarwin [
-    "--skip=store::tests::test_exclude_from_indexing_sets_tmutil_xattr"
-  ];
+  installPhase = ''
+    runHook preInstall
 
-  env.RUSTC_WRAPPER = "";
+    mkdir -p "$out/bin"
+    install -m755 kache "$out/bin/kache"
+
+    runHook postInstall
+  '';
 
   meta = {
     description = "Zero-copy, content-addressed Rust build cache";
     homepage = "https://github.com/kunobi-ninja/kache";
     license = lib.licenses.asl20;
     mainProgram = "kache";
-    platforms = lib.platforms.unix;
+    platforms = builtins.attrNames sources;
   };
 }
